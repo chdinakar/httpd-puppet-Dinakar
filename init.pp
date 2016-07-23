3e41753aba094d97a6a622e3eedfae365b90d5de
Dinakar Chikati <chdinakar@Dinakars-MacBook-Pro.local>
2016-07-23 16:14:03 -0500

    updated init.pp file

--- init.pp
+++ init.pp
@@ -1,34 +1,29 @@
 Class httpd {
 
 package { 'httpd':
-    ensure => installed,
-    before => File[‘/etc/httpd/conf/httpd.conf’],
+    ensure => 'installed',
     before => File[‘/var/www/html/index.html’],
-
 }
 
 file { '/etc/httpd/conf/httpd.conf':
-    ensure  => file, 
-    source  => 'puppet:///modules/httpd/httpd.conf/',
+    ensure  => 'file', 
+    source  => 'puppet:///modules/httpd/httpd.conf',
     owner   => 'root',
     group   => 'root',
     mode    => '644',
-    notify  => Service['httpd'], 
     require => Package['httpd'],
 }
 
 file { '/var/www/html/index.html':
-    ensure  => file, 
-    source  => 'puppet:///modules/httpd/index.html/',
+    ensure  => 'file',
+    source  => 'puppet:///modules/httpd/index.html',
     owner   => 'root',
     group   => 'root',
     mode    => '755',
-    notify  => Service['httpd'], 
-    require => Package['httpd'],
 }
 
 service { 'httpd':
-    ensure => running,
+    ensure => 'running',
     enable => true,
     hasstatus => true,
     hasrestart => true,
